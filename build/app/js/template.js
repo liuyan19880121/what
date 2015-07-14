bd84ea63688c586ac516adb47e337560bf6fa20b
liuyan19880121 <liuyan19880121@sina.com>
2015-07-15 00:45:50 +0300

    first commit

--- build/app/js/template.js
+++ build/app/js/template.js
@@ -18,13 +18,12 @@ catch(err) { module = angular.module("ngTemplate", []); }
 module.run(["$templateCache", function($templateCache) {
   "use strict";
   $templateCache.put("index.html",
-    "<div>\n" +
-    "    <div ng-include=\"'header.html'\"></div>\n" +
-    "    <div id=\"main\">\n" +
-    "        <div ng-include=\"contentTPL\" class=\"content-container\"></div>\n" +
-    "        <div ng-include=\"'sidebar.html'\" class=\"sidebar-container\"></div>\n" +
-    "    </div>\n" +
-    "</div>");
+    "<div ng-include=\"'header.html'\"></div>\n" +
+    "<div id=\"main\" class=\"wrapper\">\n" +
+    "    <div ng-include=\"contentTPL\" class=\"content-container\"></div>\n" +
+    "    <div ng-include=\"'sidebar.html'\" class=\"sidebar-container\"></div>\n" +
+    "</div>\n" +
+    "");
 }]);
 })();
 
@@ -52,7 +51,7 @@ module.run(["$templateCache", function($templateCache) {
     "        <form accept-charset=\"UTF-8\">\n" +
     "            <div class=\"edit-container\">\n" +
     "                <div class=\"topic-area\">\n" +
-    "                    <input type=\"text\" class=\"topic-title-input\" placeholder=\"标题\" ng-model=\"topic.title\"/>\n" +
+    "                    <input type=\"text\" class=\"topic-text-input\" placeholder=\"标题\" ng-model=\"topic.title\"/>\n" +
     "                </div>\n" +
     "                <div class=\"topic-content-container\" ng-init=\"editSelect=true\">\n" +
     "                    <div class=\"topic-tab-area tabnav\">\n" +
@@ -62,13 +61,13 @@ module.run(["$templateCache", function($templateCache) {
     "                        </nav>\n" +
     "                    </div>\n" +
     "                    <div ng-class=\"['topic-area',{hidden:!editSelect}]\">\n" +
-    "                        <textarea class=\"topic-title-input content-textarea\" placeholder=\"内容\" ng-model=\"topic.content\"></textarea>\n" +
+    "                        <textarea class=\"topic-text-input content-textarea\" placeholder=\"内容\" ng-model=\"topic.content\"></textarea>\n" +
     "                    </div>\n" +
     "                    <div ng-class=\"['topic-area',{hidden:editSelect}]\">\n" +
     "                        <markdown></markdown>\n" +
     "                    </div>\n" +
     "                    <div class=\"form-actions\">\n" +
-    "                        <button type=\"submit\" class=\"btn btn-primary\" ng-click=\"commit()\" ng-disabled=\"!topic.title || !topic.content\">提交</button>\n" +
+    "                        <button type=\"submit\" class=\"btn btn-primary right\" ng-click=\"commit()\" ng-disabled=\"!topic.title || !topic.content\">提交</button>\n" +
     "                    </div>\n" +
     "                </div>\n" +
     "            </div>\n" +
@@ -107,6 +106,19 @@ catch(err) { module = angular.module("ngTemplate", []); }
 module.run(["$templateCache", function($templateCache) {
   "use strict";
   $templateCache.put("topic.html",
-    "<markdown></markdown>");
+    "<div class=\"topic-title-wrapper\">\n" +
+    "	<div class=\"topic-edit-action right\">\n" +
+    "		<a href=\"/topic/edit/{{topic._id}}\" class=\"btn btn-sm btn-primary\">编辑</a>\n" +
+    "	</div>\n" +
+    "	<h1 class=\"topic-title-show\">\n" +
+    "		<span>{{topic.title}}</span>\n" +
+    "	</h1>\n" +
+    "</div>\n" +
+    "<div class=\"topic-extra_info\">\n" +
+    "	<span>发布于 1天前 • 30分钟前更新 • 作者 *** • *** 次浏览 </span>\n" +
+    "</div>\n" +
+    "<div class=\"topic-content-wrapper\">\n" +
+    "	<markdown></markdown>\n" +
+    "</div>");
 }]);
 })();
